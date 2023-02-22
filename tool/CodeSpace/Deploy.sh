219fa8d3b2e9f7e7172bef33976bc4b646892c7d
mineqwwe <mineqwwwa@outlook.com>
2023-02-22 15:13:23 +0000

    modified:   tool/CodeSpace/Deploy.sh

--- tool/CodeSpace/Deploy.sh
+++ tool/CodeSpace/Deploy.sh
@@ -1,7 +1,7 @@
 #dl jdk 8 11 17 gtadle-8.0 nim clang
 sudo apt update
 sudo apt upgrade -y
-sudo apt install wget openssl vim nim clang -y
+sudo apt install wget openssl vim unzip clang -y
 cd ~
 mkdir sdk
 cd sdk
@@ -13,11 +13,15 @@ wget https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.1
 wget https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u362-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u362b09.tar.gz
 #d_gradle-8.0
 wget https://services.gradle.org/distributions/gradle-8.0-bin.zip
+#d_nim:1.6.10
+wget https://nim-lang.org/download/nim-1.6.10-linux_x64.tar.xz
 
 #untar
 tar -xvf OpenJDK17U-jdk_x64_linux_hotspot_17.0.6_10.tar.gz
 tar -xvf OpenJDK11U-jdk_x64_linux_hotspot_11.0.18_10.tar.gz
 tar -xvf OpenJDK8U-jdk_x64_linux_hotspot_8u362b09.tar.gz
+tar -xvf nim-1.6.10-linux_x64.tar.gz
+
 unzip gradle-8.0-bin.zip
 
 #clear cache
@@ -25,12 +29,14 @@ rm OpenJDK8U-jdk_x64_linux_hotspot_8u362b09.tar.gz
 rm OpenJDK11U-jdk_x64_linux_hotspot_11.0.18_10.tar.gz
 rm OpenJDK17U-jdk_x64_linux_hotspot_17.0.6_10.tar.gz
 rm gradle-8.0-bin.zip
+rm nim-1.6.10-linux_x64.tar.gz
 
 #jdk dirs
 #jdk8u362-b09/
 #jdk-11.0.18+10/
 #jdk-17.0.6+10/
 #gradle-8.0/
+#nim-1.6.10
 
 #Android-cmdline-tools
 mkdir Android
