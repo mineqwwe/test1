76c58f87e5c575ab5c729dfbe8c789498c80756e
mineqwwe <mineqwwa@outlook.com>
2023-02-28 14:25:42 +0800

    new file:   tool/Termux/dircolors modified:   tool/Termux/zsh-addons.sh modified:   tool/Termux/zshrc

--- tool/Termux/zsh-addons.sh
+++ tool/Termux/zsh-addons.sh
@@ -1,3 +1,4 @@
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions $ZSH/plugins/zsh-autosuggestions
 cp zshrc $HOME/.zshrc
+cp dircolors $HOME/.dircolors
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting $ZSH/plugins/zsh-syntax-highlighting
